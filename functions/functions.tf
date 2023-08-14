6cb02b43fc88058fb5a7f9e2da15dd6958b107f1
SIVA LINGAM <sivalingamganaganboyina@gmail.com>
2023-08-14 10:30:33 +0530

    terraform

--- functions/functions.tf
+++ functions/functions.tf
@@ -8,6 +8,7 @@ variable "comp" {
     }
   }
 }
-output "siva" {
-  value = lookup(var.comp,"frontend",null )
-}
+#output "siva" {
+#  for_each = var.comp
+#  name = lookup(each.key,"frontend",null)
+#
