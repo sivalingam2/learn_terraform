b23d67b307e228d7c931981e30c624d9173f48a9
SIVA LINGAM <sivalingamganaganboyina@gmail.com>
2023-08-14 10:43:21 +0530

    terraform

--- roboshopv2/servers.tf
+++ roboshopv2/servers.tf
@@ -70,7 +70,7 @@ resource "aws_route53_record" "records" {
   name    = "${lookup( each.value, "name", null ) }.sivadevops22.online"
   type    = "A"
   ttl     = 30
-  records = [lookup(lookup(aws_instance.instance, each.key, null), private_ip, null )]
+  records = [lookup(lookup(aws_instance.instance, each.key, null), "private_ip", null )]
 }
 
 
