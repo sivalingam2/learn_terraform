edb7a070e113e1fab7406b7c0146be052f6cd380
SIVA LINGAM <sivalingamganaganboyina@gmail.com>
2023-08-15 11:55:47 +0530

    terraform

--- module/local-module/main.tf
+++ module/local-module/main.tf
@@ -1,7 +1,14 @@
-data "aws_ami_ids" "ami" {
-  name_regex       = "centos-*"
+data "aws_ami" "ami" {
+  name_regex       = "Centos-8-DevOps-Practice"
   owners           = ["973714476881"]
 }
 output "ami" {
-  value = data.aws_ami_ids.ami
+  value = data.aws_ami.ami
 }
+resource "aws_instance" "instance" {
+  ami           =data.aws_ami.ami.id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [ "sg-07bf8bea064a5f22c" ]
+
+}
+variable "instance_type" { }
--- module/main.tf
+++ module/main.tf
@@ -1,6 +1,10 @@
 module "test" {
   source = "./local-module"
+  instance_type = var.instance_type
 }
 output "test" {
-  value = module.test.ami
+  value = module.test
+}
+variable "instance_type" {
+  default = "t3.micro"
 }
